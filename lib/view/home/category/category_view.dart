c71041e8424809c6bac4df690bbe1443df94cc37
Your Name <eliffkaragoz00@gmail.com>
2022-08-09 03:03:55 +0300

    edit

--- lib/view/home/category/category_view.dart
+++ lib/view/home/category/category_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ui_design_2/view/home/category/category_model.dart';
-import 'package:ui_design_2/view/home/product/product_model.dart';
+import 'category_model.dart';
+import '../product/product_model.dart';
 import '../home_view.dart';
 import '../../../components/card.dart';
 import '../../../extension/context_extension.dart';
@@ -19,44 +19,6 @@ class _CategoryViewState extends State<CategoryView> {
     Category(categoryName: "Kitchen", icon: Icons.soup_kitchen_outlined),
     Category(categoryName: "Cosmetic", icon: Icons.brush_rounded),
     Category(categoryName: "Cosmetic", icon: Icons.brush_rounded),
-  ];
-
-  List productList = [
-    Product(
-        productName: "Jordon Zoom Seperate PD",
-        price: "133",
-        isFavorite: false,
-        // ignore: prefer_const_constructors
-        image: AssetImage("assets/1.jpg")),
-    Product(
-        productName: "Jordon Zoom ",
-        price: "1200",
-        isFavorite: false,
-        // ignore: prefer_const_constructors
-        image: AssetImage("assets/2.jpg")),
-    // ignore: duplicate_ignore
-    Product(
-        productName: "Nike Air Zoom",
-        price: "160",
-        isFavorite: false,
-        // ignore: prefer_const_constructors
-        image: AssetImage("assets/4.jpg")),
-    Product(
-        productName: "Tonny Black Siyah Beyaz Unisex Sneaker ",
-        price: "127",
-        isFavorite: true,
-        image: AssetImage("assets/5.jpg")),
-    Product(
-        productName: "Nike Air Zoom",
-        price: "160",
-        isFavorite: false,
-        // ignore: prefer_const_constructors
-        image: AssetImage("assets/4.jpg")),
-    Product(
-        productName: "Tonny Black Siyah Beyaz Unisex Sneaker ",
-        price: "127",
-        isFavorite: true,
-        image: AssetImage("assets/5.jpg")),
   ];
 
   @override
